9b6c78f9073dd0b897f42a635e5457e4625f20c5
Justin Tallant <jtallant07@gmail.com>
2020-02-05 18:00:29 +0000

    Apply fixes from StyleCI

--- src/SkeletonClass.php
+++ src/SkeletonClass.php
@@ -16,6 +16,7 @@ class SkeletonClass
      * Friendly welcome.
      *
      * @param string $phrase Phrase to return
+     *
      * @return string Returns the phrase passed in
      */
     public function echoPhrase($phrase)
